a2362e6dd46054733ded65272656baf9fdbb624c
Pasha <amberswcx@gmail.com>
2022-01-25 21:27:01 +0400

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  for (let i = 0; i <= str.length; i++) {
+    for (let brackets of bracketsConfig) {
+      if (brackets.includes(str[i])) {
+
+        stack.push(str[i]);
+        if (stack.slice(-2).join('') === brackets.join('')) {
+          stack.pop();
+          stack.pop();
+        }
+
+      }
+    }
+  }
+
+  return (stack.length === 0)
 }
